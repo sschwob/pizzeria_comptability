e3c841e0f37d1d4ad8fe8430b7348432eb34e468
Samuel Schwob <samuel.schwob@naomi.fr>
2024-03-04 13:51:23 +0100

    feat(balanceSheet): add validation

--- app/models/balance_sheet.rb
+++ app/models/balance_sheet.rb
@@ -3,4 +3,14 @@ class BalanceSheet < ApplicationRecord
 
   validates :month, presence: true
   validates :turnover, presence: true
+
+  validate :one_turnover_per_month_per_restaurant
+
+  def one_turnover_per_month_per_restaurant
+    if self.class.where(restaurant_id: self.restaurant_id)
+                 .where('EXTRACT(MONTH FROM month) = ? AND EXTRACT(YEAR FROM month) = ?', self.month.month, self.month.year)
+                 .any?
+      errors.add(:month, "balance_sheet #{self.month.year}-#{self.month.month} already exists for pizzeria_id #{self.restaurant_id}")
+    end
+  end 
 end
